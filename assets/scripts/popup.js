6e8726eef01d95fcb0ce2081d6ff384ca6831c6b
Olesya Blizniuk <olesyablizniuk@gmail.com>
2023-04-16 20:30:03 +0600

    fix: change popup

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -8,25 +8,19 @@ const openPopup = (event) => {
 
   popup.classList.add('open');
   document.body.classList.add('no-scroll');
-
   const imgPet = document.querySelector('.img_pet');
   const petName = document.querySelector('.pet_name');
   const petBreed = document.querySelector('.pet_breed');
   const petDescription = document.querySelector('.pet_description');
   const petAge = document.querySelector('#pet_age'); 
-
   const petInoculations = document.querySelector('#pet_inoculations'); 
   const petDiseases= document.querySelector('#pet_diseases'); 
   const petParasites = document.querySelector('#pet_parasites'); 
-
-
   imgPet.src = pet.img;
   petName.textContent = pet.name;
   petBreed.textContent = pet.breed;
   petDescription.textContent = pet.description;
   petAge.textContent = pet.age;
-
-
   petInoculations.textContent = pet.inoculations;
   petDiseases.textContent = pet.diseases;
   petParasites.textContent = pet.parasites;
@@ -47,7 +41,7 @@ window.addEventListener('keydown', (e) => {
   });
 
 // Закрыть модальное окно при клике вне его
-document.querySelector("#popup .modal_window").addEventListener('click', event => {
+document.querySelector("#popup .modal_card").addEventListener('click', event => {
     event._isClickWithInModal = true;
   });
   popup.addEventListener('click', event => {
